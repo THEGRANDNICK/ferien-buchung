3031cfbafff2d481ba3fdfe69c1ada014082119e
Nicolas Vogt <100305591+THEGRANDNICK@users.noreply.github.com>
2025-05-07 10:45:19 +0200

    add picture and style change

---
+++ js/theme.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("toggle-dark").addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+  });
+});
